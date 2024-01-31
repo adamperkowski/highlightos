6ffc8ff7a0683c6bdd0ab190a06ead0c8750a251
Adam Perkowski <adas1per@protonmail.com>
2024-01-31 01:58:28 +0100

    initial commit

---
+++ boot.asm
@@ -0,0 +1,30 @@
+call cls
+
+[org 0x7c00]
+mov ah, 0x0e
+mov bx, text
+
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp printString
+end:
+
+text:
+    db "HighlightOS", 0
+
+jmp $
+
+cls:
+  pusha
+  mov ah, 0x00
+  mov al, 0x03
+  int 0x10
+  popa
+  ret
+
+times 510-($-$$) db 0
+dw 0xaa55
