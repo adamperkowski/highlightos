4dcc902f2820505495c0ebec363ea9205238578e
Adam Perkowski <adas1per@protonmail.com>
2024-01-31 12:28:31 +0100

    a lil arrangement

--- boot.asm
+++ boot.asm
@@ -5,20 +5,14 @@ call cls
 [org 0x7c00]
 mov ah, 0x0e
 mov bx, text
-
-printString:
-    mov al, [bx]
-    cmp al, 0
-    je end
-    int 0x10
-    inc bx
-    jmp printString
-end:
+call printString
 
 text:
-    db "HighlightOS", 13, 10, 10, " Copyright (C) 2024", 13, 10, " Adam Perkowski", 0
+  db 13, 10, "HighlightOS", 13, 10, 10, " Copyright (C) 2024", 13, 10, " Adam Perkowski", 0
+
 
 jmp $
+
 
 cls:
   pusha
@@ -27,6 +21,18 @@ cls:
   int 0x10
   popa
   ret
+
+printString:
+  pusha
+  mov al, [bx]
+  cmp al, 0
+  je end
+  int 0x10
+  inc bx
+  jmp printString
+  popa
+  ret
+end:
 
 times 510-($-$$) db 0
 dw 0xaa55
