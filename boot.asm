5640e2d79619302c70810b88a94f03024cde23a6
Adam Perkowski <adas1per@protonmail.com>
2024-01-31 15:14:16 +0100

    backspace

--- boot.asm
+++ boot.asm
@@ -19,9 +19,16 @@ shell:
   mov al, '>'
   call nl
 
+  mov cl, 0
+
+  jmp input
+
   input:
     mov ah, 0
     int 0x16
+
+    cmp al, 0x08
+    je bs
 
     cmp al, 0x0D
     jne ps
@@ -59,29 +66,41 @@ nl:
   mov ah, 0x0e
   int 0x10
 
-  mov ah, 0x0e
   mov al, 10
   int 0x10
-  mov ah, 0x0e
   mov al, 13
   int 0x10
-  mov ah, 0x0e
   mov al, 10
   int 0x10
-  mov ah, 0x0e
   mov al, 13
   int 0x10
-  mov ah, 0x0e
   mov al, '<'
   int 0x10
 
   ret
 
 ps:
-    mov ah, 0x0e
-    int 0x10
-    jmp input
-    ret
+  mov ah, 0x0e
+  int 0x10
+  inc cl
+  jmp input
+  ret
+ps1:
+  mov ah, 0x0e
+  mov al, 0x08
+  int 0x10
+  mov al, ' '
+  int 0x10
+  mov al, 0x08
+  int 0x10
+  dec cl
+  jmp input
+  ret
+
+bs:
+  cmp cl, 0
+  je input
+  jne ps1
 
 times 510-($-$$) db 0
 dw 0xaa55
