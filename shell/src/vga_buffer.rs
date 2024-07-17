f24990e7eabcb1a040cd9003c26be268c40d92db
Adam Perkowski <adas1per@protonmail.com>
2024-07-17 18:16:26 +0200

    fixed.

--- shell/src/vga_buffer.rs
+++ shell/src/vga_buffer.rs
@@ -89,9 +89,7 @@ impl Writer {
     fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
-                // printable ASCII byte or newline
                 0x20..=0x7e | b'\n' => self.write_byte(byte),
-                // not part of printable ASCII range
                 _ => self.write_byte(0xfe),
             }
         }
