f8579547d11b91073c1106dbe2c5b5b3d8b98daf
Adam Perkowski <adas1per@protonmail.com>
2024-07-17 13:08:02 +0200

    Booting works

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,15 +1,14 @@
-#![feature(global_asm)]
 #![no_main]
 #![no_std]
 
-global_asm!(r#"
-loop:
-  jmp loop
-.org 510
-.word 0xaa55
-"# :::: "volatile");
+use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(panic_info: &core::panic::PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
