482eca6f37a6cd3f2df380660540eddb40cb210b
Adam Perkowski <adas1per@protonmail.com>
2024-07-18 01:35:04 +0200

    MERGE Make hlshell bootable and get rid of std (#4)
    This includes creating a simple bootloader in Rust, getting rid of
    (converting it all to inline assembly) etc.
    I think I'm gonna kinda merge the assembly and rust part of Highlight
    OS here.

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,67 +1,20 @@
-// Copyleft 🄯 2024  Adam Perkowski
+#![no_main]
+#![no_std]
+#![allow(clippy::empty_loop)]
 
-use std::io;
-use std::io::Write;
+use core::panic::PanicInfo;
 
-mod commands;
+mod vga_buffer;
 
-use commands::COMMAND_LIST;
-
-struct RtrType {
-    code: &'static i32,
-    info: &'static str,
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
 
-fn main() {
-    // (COMMAND_LIST[0].fun)();
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("HighlightOS Shell v0.3.0\n\nhls < ");
 
-    println!("HighlightOS Shell\nversion {}\n", env!("CARGO_PKG_VERSION"));
-
-    loop {
-        let mut inpt = String::new();
-
-        print!("hls < ");
-
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut inpt).unwrap();
-
-        inpt.pop();
-
-        let mut args: Vec<&str> = inpt.split(' ').collect();
-
-        if let Some(command) = COMMAND_LIST.iter().find(|&cmd| cmd.name == args[0]) {
-            args.remove(0);
-
-            let rtr = (command.fun)(args);
-
-            if rtr != 1 {
-                if let Some(return_code) = RTR_LIST.iter().find(|&rtr_t| rtr_t.code == &rtr) {
-                    println!("\n > {}\n{}\n", inpt, return_code.info);
-                } else {
-                    println!("\n > {}\nreturned : {}\n", inpt, rtr)
-                }
-            }
-        } else {
-            println!("\n > {}\ncommand not found\n", inpt);
-        }
-    }
+    loop {}
 }
-
-const RTR_LIST: &[RtrType] = &[
-    RtrType {
-        code: &0,
-        info: "executed successfully",
-    },
-    RtrType {
-        code: &2,
-        info: "returned general error",
-    },
-    RtrType {
-        code: &3,
-        info: "returned critical error",
-    },
-    RtrType {
-        code: &4,
-        info: "returned user error",
-    },
-];
