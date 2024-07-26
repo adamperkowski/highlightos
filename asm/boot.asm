22ce0874ba7688f9fbc29f1267fe9a92f9d81fc4
franzageek <88248950+franzageek@users.noreply.github.com>
2024-07-26 10:42:21 +0200

    Update boot.asm
    - Refactor the code (.text and .data section);
    - Improve label naming;
    - Remove jumps before 'ret's;
    
    Signed-off-by: franzageek <88248950+franzageek@users.noreply.github.com>

--- asm/boot.asm
+++ asm/boot.asm
@@ -1,17 +1,18 @@
 ; Copyleft 🄯 2024  Adam Perkowski
+[org 0x7c00]
+bits 16
+section .text
 
 call cls
+xor ax, ax
+mov ds, ax
+mov es, ax
+mov si, boot_msg
+call print
 
-[org 0x7c00]
-  xor ax, ax
-  mov ds, ax
-  mov es, ax
-  mov si, msg
-  call print
+jmp shell
 
-  jmp shell
-
-  jmp $
+jmp $
 
 
 shell:
@@ -25,15 +26,15 @@ shell:
   mov bx, 2
   mov cl, 0
 
-  input:
-    mov ah, 0
-    int 0x16
+.input:
+  mov ah, 0
+  int 0x16
 
-    cmp al, 0x08
-    je bs
+  cmp al, 0x08
+  je .buffer_bounds
 
-    cmp al, 0x0D
-    jne ps
+  cmp al, 0x0D
+  jne .print_input
 
   mov [buffer], byte 0x0a
   mov [buffer + 1], byte 0x0d
@@ -41,11 +42,11 @@ shell:
   mov ah, 0x0e
   mov bx, buffer
 
-  jmp loopPrint
+  jmp print_loop
 
   jmp shell
 
-ps:
+.print_input:
   mov ah, 0x0e
   int 0x10
   inc cl
@@ -53,9 +54,9 @@ ps:
   mov [buffer + bx], al
   inc bx
 
-  jmp input
   ret
-ps1:
+
+.backspace:
   mov ah, 0x0e
   
   mov al, 0x08
@@ -69,52 +70,48 @@ ps1:
   dec bx
   dec cl
 
-  jmp input
   ret
 
-bs:
+.buffer_bounds:
   cmp cl, 0
-  je input
-  jne ps1
-
-msg:
-  db 13, 10, "HighlightOS v0.0.1", 13, 10, 10, " Copyright (C) 2024", 13, 10, " Adam Perkowski", 0
-nl:
-  db 13, 10, 10, "hls <", 0
+  je .input
+  jne .backspace
 
 print:
   lodsb
   or al, al
-  jz end
+  jz .end
   mov ah, 0x0e
   int 0x10
   jmp print
-end:
+
+.end:
   ret
 
-loopPrint:
+print_loop:
   mov al, [bx]
   cmp al, 0
-  je clearbuffer
+  je .clear_buffer
 
   int 0x10
   inc bx
 
-  jmp loopPrint
-clearbuffer:
+  jmp print_loop
+
+.clear_buffer:
   mov al, 0
   mov [buffer + bx], al
   mov bx, 0
-  nullchar:
-    inc bx
-    cmp bx, 258
-    je endPrint
-    mov al, 0
-    mov [buffer + bx], al
 
-    jmp nullchar
-endPrint:
-  jmp shell
+.null_char:
+  inc bx
+  cmp bx, 258
+  je .end_print
+  mov al, 0
+  mov [buffer + bx], al
+  jmp .null_char
+
+.end_print:
   ret
 
 cls:
@@ -124,6 +121,12 @@ cls:
   int 0x10
   popa
   ret
+
+section .data
+boot_msg:
+  db 13, 10, "HighlightOS v0.0.1", 13, 10, 10, " Copyright (C) 2024", 13, 10, " Adam Perkowski", 0
+nl:
+  db 13, 10, 10, "hls <", 0
 
 buffer times 258 db 0
 
