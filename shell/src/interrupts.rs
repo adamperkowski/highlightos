6c1a1023daec8b9d16c6dd08ea9c5f8e31ba3a6d
Adam Perkowski <adas1per@protonmail.com>
2024-07-27 17:28:03 +0200

    Collapse if statement

--- shell/src/interrupts.rs
+++ shell/src/interrupts.rs
@@ -127,14 +127,12 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
                     }
 
                     DecodedKey::RawKey(key) => {
-                        if key == KeyCode::F1 {
-                            if unsafe { BUFFER_INDEX } > 0 {
-                                keyboard_buffer::clear_buffer();
-                                print!("\n");
-                                unsafe {
-                                    BUFFER[BUFFER_INDEX] = '\n';
-                                    BUFFER_INDEX += 1;
-                                }
+                        if key == KeyCode::F1 && unsafe { BUFFER_INDEX } > 0 {
+                            keyboard_buffer::clear_buffer();
+                            print!("\n");
+                            unsafe {
+                                BUFFER[BUFFER_INDEX] = '\n';
+                                BUFFER_INDEX += 1;
                             }
                         }
                     }
